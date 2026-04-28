5bec2368a51954d42fc801e8e2780afda2fb6fa4
Dikshant Agrawal <126512366+daniellowho@users.noreply.github.com>
2026-04-28 09:10:02 +0530

    Delete backend/src/main directory

--- backend/src/main/java/com/eventmanagement/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.eventmanagement.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
